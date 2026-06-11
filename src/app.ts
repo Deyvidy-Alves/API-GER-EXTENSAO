b8660f88119f7c8d6c15539f303bd3e0ed3bc45a
oliguilherme <guilhermeleitesouza2018@gmail.com>
2026-06-11 20:29:06 -0300

    refactor: mudanca na estrutura da criacao de usuarios

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import { AuthRoutes } from './modules/auth/auth.routes.js';
 import { ProfileRoutes } from './modules/profile/profile.routes.js';
-import { UserRoutes } from './modules/users/user.routes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use('/autenticacao', AuthRoutes);
 app.use('/perfil', ProfileRoutes);
-app.use('/usuarios', UserRoutes);
+
 
 app.get('/health', (req, res) => {
 	res.status(200).json({
