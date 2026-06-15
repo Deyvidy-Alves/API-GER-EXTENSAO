5ddd29aa12116caac65af84179512f7c635f6d49
Deyvidy-Alves <deyvidyalvessilva@gmail.com>
2026-06-15 19:11:34 -0300

    feat: add rotas professor

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { AuthRoutes } from './modules/auth/auth.routes.js';
 import { ProfileRoutes } from './modules/profile/profile.routes.js';
+import { ProfessorRoutes } from './modules/professor/professor.routes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use('/autenticacao', AuthRoutes);
 app.use('/perfil', ProfileRoutes);
+app.use('/professor', ProfessorRoutes);
 
 
 app.get('/health', (req, res) => {
