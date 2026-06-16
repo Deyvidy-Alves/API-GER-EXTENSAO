96b9cb2311aefae7e16c85482fe8ac6c949eeaaf
RUANIFCE <ruan.goncalves08@aluno.ifce.edu.br>
2026-06-16 18:53:52 -0300

    delecao de pasta

--- API-GER-EXTENSAO-MAIN/src/modules/profile/profile.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const StudentProfileSchema = z.object({
-  matricula: z.string().min(1, 'Matricula obrigatória'),
-  cursoGraduacao: z.string().min(1, 'Curso obrigatório'),
-  semestre: z.int().min(1).max(10).optional()
-});
-
-export type StudentProfilelDTO = z.infer<typeof StudentProfileSchema>
--- API-GER-EXTENSAO-MAIN/src/utils/getEnv.ts
+++
@@ -1,9 +0,0 @@
-export function getEnv(key: string): string {
-  const value = process.env[key];
-  
-  if (!value) {
-    throw new Error(`Environment variable ${key} is not defined.`);
-  }
-
-  return value;
-}
