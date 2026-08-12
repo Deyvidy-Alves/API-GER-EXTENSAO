267261cc3ec2c48fff1c3d029e40f90085942255
Tayson09 <tayson.silva07@aluno.ifce.edu.br>
2026-08-12 16:49:28 -0300

    feat: add DeppiRoutes to app for improved routing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 import path from 'path';
-<<<<<<< HEAD
-=======
-
->>>>>>> 58c295bd48d4b72df7816e1e7e99b3512ffa4e92
 import { AuthRoutes } from './modules/auth/auth.routes.js';
 import { ProfileRoutes } from './modules/profile/profile.routes.js';
 import { CursosRoutes } from './modules/cursos/cursos.routes.js';
 import { ProfessorRoutes } from './modules/professor/professor.routes.js';
 import SubRoutes from './modules/subscription/subscription.routes.js';
+import { DeppiRoutes } from "./modules/deppi/deppi.routes.js";
 
 const app = express();
 
@@ -20,6 +17,7 @@ app.use('/cursos', CursosRoutes);
 app.use('/professor', ProfessorRoutes);
 app.use('/inscricao', SubRoutes);
 app.use('/uploads', express.static(path.resolve('uploads')));
+app.use("/deppi", DeppiRoutes);
 
 app.use('/uploads', express.static(path.resolve(process.cwd(), 'uploads')));
 
